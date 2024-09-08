22152e3b1983aaa864e0d35cb1bd602ce91d4f27
Ayushksingh77 <113466690+Ayushksingh77@users.noreply.github.com>
2024-09-08 16:13:53 +0530

    some changes in app js
    optimised code

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-let navtoggle = document.querySelector(".nav-toggle");
-let sidebar = document.querySelector(".sidebar");
+document.addEventListener("DOMContentLoaded", function() {
+    let navtoggle = document.querySelector(".nav-toggle");
+    let sidebar = document.querySelector(".sidebar");
 
-navtoggle.addEventListener("click",function(){
-    if(navtoggle.firstElementChild.classList.contains("fa-bars"))
-    {
-        navtoggle.firstElementChild.classList.replace("fa-bars","fa-times");
-
-    }
-    else{
-        navtoggle.firstElementChild.classList.replace("fa-times","fa-bars");
-    }
-    sidebar.classList.toggle("show-sidebar");
-})
+    navtoggle.addEventListener("click", function() {
+        if (navtoggle.firstElementChild.classList.contains("fa-bars")) {
+            navtoggle.firstElementChild.classList.replace("fa-bars", "fa-times");
+        } else {
+            navtoggle.firstElementChild.classList.replace("fa-times", "fa-bars");
+        }
+        sidebar.classList.toggle("show-sidebar");
+    });
+});
